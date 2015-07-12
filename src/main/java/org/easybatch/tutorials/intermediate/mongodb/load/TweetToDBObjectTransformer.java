35a5392e87c1b08345cc4724ec769cc097ac22dd
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-07-12 08:27:07 +0200

    use MongoDBRecordWriter in MongoDB tutorial

--- src/main/java/org/easybatch/tutorials/intermediate/mongodb/load/TweetToDBObjectTransformer.java
+++ src/main/java/org/easybatch/tutorials/intermediate/mongodb/load/TweetToDBObjectTransformer.java
@@ -25,35 +25,23 @@
 package org.easybatch.tutorials.intermediate.mongodb.load;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
 import org.easybatch.core.api.RecordProcessor;
 import org.easybatch.tutorials.common.Tweet;
 
 /**
- * A record processor that inserts tweets in MongoDB.
+ * A record processor that transforms a tweet into a MongoDB Object.
  *
  * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
  */
-public class TweetLoader implements RecordProcessor<Tweet, Tweet> {
-
-    private DBCollection tweetsCollection;
-
-    public TweetLoader(MongoClient client, String database,  String collection) {
-        tweetsCollection = client.getDB(database).getCollection(collection);
-    }
+public class TweetToDBObjectTransformer implements RecordProcessor<Tweet, DBObject> {
 
     @Override
-    public Tweet processRecord(Tweet tweet) {
+    public DBObject processRecord(Tweet tweet) {
 
-        DBObject dbObject = new BasicDBObject()
+        return new BasicDBObject()
                 .append("_id", tweet.getId())
                 .append("user", tweet.getUser())
                 .append("message", tweet.getMessage());
-
-        tweetsCollection.insert(dbObject);
-
-        return tweet;
     }
 }
