d3d75b369c272d41dd7b369571dfc5ff83fe2fa0
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-07-15 13:56:22 +0200

    add tutorial about chunk processing

---
+++ src/main/java/org/easybatch/tutorials/intermediate/chunks/Launcher.java
@@ -0,0 +1,42 @@
+package org.easybatch.tutorials.intermediate.chunks;
+
+import org.easybatch.core.api.RecordProcessingException;
+import org.easybatch.core.api.RecordProcessor;
+import org.easybatch.core.reader.IterableMultiRecordReader;
+import org.easybatch.core.record.MultiRecord;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.easybatch.core.impl.EngineBuilder.aNewEngine;
+
+/**
+ * Main class to launch the chunk processing tutorial.
+ *
+ * This example shows the usage of {@link IterableMultiRecordReader} that reads records in chunks from an iterable data source
+ *
+ * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ */
+public class Launcher {
+
+    public static final int CHUNK_SIZE = 2;
+
+    public static void main(String[] args) throws Exception {
+
+        List<String> dataSource = Arrays.asList("foo", "bar", "baz", "toto", "titi");
+
+        aNewEngine()
+                .reader(new IterableMultiRecordReader<String>(CHUNK_SIZE, dataSource))
+                .processor(new MultiRecordProcessor())
+                .build().call();
+    }
+
+    private static class MultiRecordProcessor implements RecordProcessor<MultiRecord, MultiRecord> {
+
+        @Override
+        public MultiRecord processRecord(MultiRecord multiRecord) throws RecordProcessingException {
+            System.out.println(multiRecord);
+            return multiRecord;
+        }
+    }
+}
